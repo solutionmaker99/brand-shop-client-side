8e8a050e306398b5bf76787bb00d4f40e146f939
solugionmaker99 <frriyad111@gmail.com>
2023-10-20 18:11:22 +0600

    added

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -2,7 +2,9 @@ import {
   createUserWithEmailAndPassword,
   onAuthStateChanged,
   signInWithEmailAndPassword,
+  GoogleAuthProvider,
   signOut,
+  signInWithPopup,
 } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
 import auth from "../firebase/firebase.config";
@@ -12,12 +14,18 @@ export const AuthContext = createContext(null);
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
+  const googleProvider = new GoogleAuthProvider();
+
   const signUpUser = (email, password) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
   const signInUser = (email, password) => {
     return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const googlePopUp = () => {
+    return signInWithPopup(auth, googleProvider);
   };
 
   useEffect(() => {
@@ -34,7 +42,7 @@ const AuthProvider = ({ children }) => {
     return signOut(auth);
   };
 
-  const authInfo = { user, signUpUser, signInUser, logOut };
+  const authInfo = { user, signUpUser, signInUser, googlePopUp, logOut };
 
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
