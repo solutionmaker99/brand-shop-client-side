afcf0a75fd89b7196250e453ebb2affd3ffd8a8b
solugionmaker99 <frriyad111@gmail.com>
2023-10-22 12:38:43 +0600

    slider added on pagedetails route

--- src/components/Home/ProductDetail.jsx
+++ src/components/Home/ProductDetail.jsx
@@ -1,5 +1,10 @@
-import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 import { Link, useLoaderData, useParams } from "react-router-dom";
+import slide1 from "../../assets/slide1.jpg";
+import slide2 from "../../assets/slide2.jpg";
+import slide3 from "../../assets/slide3.jpg";
+import slide4 from "../../assets/slide4.jpg";
 
 const ProductDetail = () => {
   const { id } = useParams();
@@ -8,6 +13,28 @@ const ProductDetail = () => {
   console.log(id);
   return (
     <div>
+      <div>
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={3}
+          onSlideChange={() => console.log("slide change")}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          <SwiperSlide>
+            <img className="w-[100vw] h-[50vh]" src={slide1} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="w-[100vw] h-[50vh]" src={slide2} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="w-[100vw] h-[50vh]" src={slide3} alt="" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className="w-[100vw] h-[50vh]" src={slide4} alt="" />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+
       <div className="card card-compact w-96 bg-base-100 shadow-xl">
         <figure>
           <img src={photo} alt="Product" />
